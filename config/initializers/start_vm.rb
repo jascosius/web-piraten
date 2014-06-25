5484adcc4dfc5019f82ad5df64964655fe742b2e
Marius Rasch <mras@informatik.uni-kiel.de>
2014-06-25 12:55:11 +0200

    update vm to use in qemu add startscript for production refs #976

--- config/initializers/start_vm.rb
+++ config/initializers/start_vm.rb
@@ -1,8 +1,15 @@
+PATH_TO_VMSCRIPT= '/home/marius/VMs/qemu/control.sh'
+
 if Rails.env.development?
   Thread.start do
     puts 'Started virtual machine for simulation (development only)'
     # blocking the thread
-    system('ruby ./app/vm/vm.rb')
+    system('ruby ./vm/vm/vm.rb development')
     puts 'Stopped virtual machine for simulation'
+  end
+elsif Rails.env.production?
+  Thread.start do
+    puts 'Started virtual machine in qemu (for external use)'
+    system(PATH_TO_VMSCRIPT, 'use')
   end
 end
