6e0026984f6d6be1bbd5b4998ca9aa2e91f69cc4
Björn Peemöller <bjp@informatik.uni-kiel.de>
2014-07-14 14:51:54 +0200

    Added Capistrano configuration

---
+++ config/deploy/production.rb
@@ -0,0 +1,5 @@
+set :branch      , 'master'
+set :application , 'web-piraten'
+set :sub_uri     , '/web-piraten'
+set :deploy_to   , "/var/lib/www-rails/#{fetch(:application)}"
+set :rails_env   , :production
