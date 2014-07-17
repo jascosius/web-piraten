9d5535c6fef37abc6ee31b204631f4fdfe562f8d
Marius Rasch <mras@informatik.uni-kiel.de>
2014-07-17 12:02:25 +0200

    fixes start_vm.rb

--- config/initializers/start_vm.rb
+++ config/initializers/start_vm.rb
@@ -1,15 +1,7 @@
-PATH_TO_VMSCRIPT= '/home/marius/VMs/qemu/control.sh'
-
 if Rails.env.development?
   Thread.start do
     puts 'Started virtual machine for simulation (development only)'
     # blocking the thread
     Thread.new {system('ruby -C vm/vm vm.rb development')}
-  end
-else
-  Thread.start do
-    puts 'Started virtual machine for simulation (production)'
-    # blocking the thread
-    Thread.new {system('ruby -C vm/vm vm.rb production')}
   end
 end
