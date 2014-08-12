3dfe3a5d282dd232c73800a5615dc8042b7d739d
Mathis Neumann <mne@informatik.uni-kiel.de>
2014-08-12 17:57:48 +0200

    use thin as server

--- config/deploy.rb
+++ config/deploy.rb
@@ -20,7 +20,7 @@ set :branch, 'production'
 
 # Manually create these paths in shared/ (eg: shared/config/database.yml) in your server.
 # They will be linked in the 'deploy:link_shared_paths' step.
-set :shared_paths, ['config/database.yml', 'log']
+set :shared_paths, ['config/', 'log']
 
 set :rvm_path, '/usr/local/rvm/scripts/rvm'
 # Optional settings:
