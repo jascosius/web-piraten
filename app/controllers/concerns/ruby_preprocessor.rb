edeab6df3a23c89069868fd4fed03af59045d3ed
amf <amf@informatik.uni-kiel.de>
2014-08-12 09:43:12 +0200

    Adds an implementation of the processing for multiline string and case blocks.

--- app/controllers/concerns/ruby_preprocessor.rb
+++ app/controllers/concerns/ruby_preprocessor.rb
@@ -16,70 +16,323 @@ class RubyPreprocessor < BasePreprocessor
     @execute_error = ''
   end
 
-  # Method that preprocesses the given code and includes the debug information
+  # Method that processes the given code and includes the debug information
   # if the user wants to trace specified variables. Furthermore it handles the
-  # given code differently, when there is a correct case-statement by checking
-  # the user's code of a valid one with a regular expression.
+  # given code differently, when there are multline strings or correct case-
+  # statements. This'll be verified by checking the user's code with regular
+  # expressions.
   def process_code(code_msg, vars)
     i=1
     codes = ''
+    bools = {}
+    bools[:single_q] = false
+    bools[:double_q] = false
+    bools[:dont_skip_line] = true
     if code_msg =~ regex_verify_case_statement
-      dont_skip_line = true
+      codes = case_block_processsing(code_msg, vars, i, '', bools)
+    elsif code_msg =~ /(?:'(?:.*(?:\n|\r|\r\n).*)+'|"(?:.*(?:\n|\r|\r\n).*)+")/
+      # Regular expression that verifies the existence of a multiline string in
+      # the code.
+      #codes = multiline_processing(code_msg, vars, i, '')
       code_msg.each_line do |s|
-        if dont_skip_line
-          codes += insert_line_number(i)
-          codes += add_user_code(s, i)
-          # match 'case casevariable # comment'
-          if s =~ /^\s*case\s+(\w+|'\w*'|"\w*")\s*((#+.*\s*)*)?$/
-            dont_skip_line = false
-          else
-            codes = insert_debug_information(codes, vars)
-          end
-          # match 'when case # comment'
-        elsif s =~ /^\s*when\s+(\w+|'\w*'|"\w*")\s*(#+.*)?$/
-          codes += add_user_code(s, i)
-          dont_skip_line = true
-        else
-          codes += add_user_code(s, i)
-        end
+        codes += insert_line_number(i, bools[:dont_skip_line])
+        bools = multiline_processing(s, bools)
+        codes += add_user_codeline(s, i, bools[:dont_skip_line])
+        codes = insert_debug_information(codes, vars, bools[:dont_skip_line])
         i += 1
       end
-    else # If there is no case-statement in the user's code, process code normally.
+    else
+      codes = ''
       code_msg.each_line do |s|
-        codes += insert_line_number(i)
-        codes += add_user_code(s, i)
-        codes = insert_debug_information(codes, vars)
+        codes += insert_line_number(i, true)
+        codes += add_user_codeline(s, i, true)
+        codes = insert_debug_information(codes, vars, true)
         i += 1
       end
     end
     insert_logic + codes + "\n"
   end
 
+  # Method that processes the code, if there is a case block in the user's code.
+  # The code is processed line by line and the further insertion of information is
+  # controlled by booleans and verifying regular expressions.
+  # Still missing: multiline strings outside the case block and multiline when
+  # statements.
+  def case_block_processsing(code_msg, vars, i, codes, bools)
+    no_multiline_case_statement = true
+    multiline_string = false
+    found_case = false
+    found_when = false
+    single_q = false
+    double_q = false
+    code_msg.each_line do |s|
+      codes += insert_line_number(i, bools[:dont_skip_line])
+      if multiline_string # processes a found multiline string
+        bools = multiline_processing(s, bools)
+        if bools[:dont_skip_line] && s =~ regex_verify_when_after_multiline
+          multiline_string = false
+          found_case = true
+          found_when = true
+          bools[:dont_skip_line] = false
+          if s =~ /when\s*(?:#+.*)?$/
+          elsif s =~ regex_verify_when_sq
+            single_q = true
+          elsif s =~ regex_verify_when_dq
+            double_q = true
+          elsif s =~ regex_verify_when_complete
+            found_case = false
+            found_when = false
+            bools[:dont_skip_line] = true
+          end
+        elsif bools[:dont_skip_line]
+          multiline_string = false
+        end
+      elsif !found_case && !found_when # Standard setting for processing of
+        # 'normal' code unless other flags are set.
+        if s =~ regex_verify_case_when
+          if s =~ regex_verify_when_sq
+            single_q = true
+            bools[:dont_skip_line] = false
+            found_case = true
+            found_when = true
+          elsif s =~ regex_verify_when_dq
+            double_q = true
+            bools[:dont_skip_line] = false
+            found_case = true
+            found_when = true
+          elsif s =~ regex_verify_when_complete
 
-  # Regularexpression for validating case-statements, even those that have
-  # comments and line breaks between them.
+          else
+            found_case = true
+            found_when = true
+            bools[:dont_skip_line] = false
+          end
+        elsif s =~ /;?\s*case\s*/
+          bools[:dont_skip_line] = false
+          found_case = true
+          if s =~ /case\s*(?:'(?:[^']?(?:\\')?)*)$/ # verifies multiline string
+            single_q = true
+            no_multiline_case_statement = false
+          elsif s =~ /case\s*(?:"(?:[^"]?(?:\\")?)*)$/ # verifies multiline string
+            double_q = true
+            no_multiline_case_statement = false
+          end
+        elsif s =~ /;?\s*\w*[!\?]*\s*=\s*(?:"(?:[^"]*(?:\\")?)*|'(?:[^']*(?:\\')?)*)$/
+          # verifies multiline string
+          bools = multiline_processing(s, bools)
+          multiline_string = true
+        elsif s =~ /'.*when.*'/
+          # Prevents the word 'when' in single-line strings to be mistaken for a when
+          # of a case statement.
+
+        elsif s =~ /".*when.*"/
+
+        elsif s =~ /when/
+          found_case = true
+          found_when = true
+          bools[:dont_skip_line] = false
+          if s =~ /when\s*(?:#+.*)?$/
+          elsif s =~ regex_verify_when_sq
+            single_q = true
+          elsif s =~ regex_verify_when_dq
+            double_q = true
+          elsif s =~ regex_verify_when_complete
+            found_case = false
+            found_when = false
+            bools[:dont_skip_line] = true
+          end
+        end
+      elsif found_case && !found_when # beginning of a case statement has been found
+        if !no_multiline_case_statement # found a multiline string for statement
+          if single_q && s =~ /(?:[^']?(?:\\')?)*[^\\]?'/
+            single_q = false
+            no_multiline_case_statement = true
+            if s =~ /when\s*(?:#+.*)?$/
+              found_when = true
+            elsif s =~ regex_verify_when_sq
+              single_q = true
+              found_when = true
+            elsif s =~ regex_verify_when_dq
+              double_q = true
+              found_when = true
+            elsif s =~ regex_verify_when_complete
+              bools[:dont_skip_line] = true
+              found_case = false
+            end
+          elsif double_q && s =~ /(?:[^"]?(?:\\")?)*[^\\]?"\s*/
+            double_q = false
+            no_multiline_case_statement = true
+            if s =~ /when\s*(?:#+.*)?$/
+              found_when = true
+            elsif s=~ regex_verify_when_sq
+              single_q = true
+              found_when = true
+            elsif s =~ regex_verify_when_dq
+              double_q = true
+              found_when = true
+            elsif s =~ regex_verify_when_complete
+              bools[:dont_skip_line] = true
+              found_case = false
+            end
+          end
+        elsif s =~ /\s*(?::*\w+\s|'(?:[^']?(?:\\')?)*'|"(?:[^"]?(?:\\")?)*")?\s*when/
+          # found rest of case statement and the following when
+          if s =~ /when\s*(?:#+.*)?$/
+            found_when = true
+          elsif s=~ regex_verify_when_sq
+            single_q = true
+            found_when = true
+          elsif s =~ regex_verify_when_dq
+            double_q = true
+            found_when = true
+          elsif s =~ regex_verify_when_complete
+            bools[:dont_skip_line] = true
+            found_case = false
+          end
+        elsif s =~ /(?:'(?:[^']?(?:\\')?)*)$/ # verifies beginning of case multiline string, single quoted
+          single_q = true
+          no_multiline_case_statement = false
+        elsif s =~ /(?:"(?:[^"]?(?:\\")?)*)$/ # verifies beginning of case multiline string, double quoted
+          double_q = true
+          no_multiline_case_statement = false
+        elsif s =~ /when/
+          found_when = true
+        end
+      elsif found_case && found_when
+        if single_q && s =~ /(?:[^']?(?:\\')?)*[^\\]?'\s*(?:#+.*)?$/
+          # verifies end of multiline string, single quotes
+          found_case = false
+          found_when = false
+          single_q = false
+          bools[:dont_skip_line] = true
+        elsif double_q && s =~ /(?:[^"]?(?:\\")?)*[^\\]?"\s*(?:#+.*)?$/
+          # verifies end of multiline string, double quotes
+          found_case = false
+          found_when = false
+          double_q = false
+          bools[:dont_skip_line] = true
+        elsif double_q || single_q
+
+        elsif s=~ /(?:\s:*\w+\s|'(?:[^']?(?:\\')?)*'|"(?:[^"]?(?:\\")?)*")\s*(?:#.*)?/
+          # verifies end of first when statement
+          found_case = false
+          found_when = false
+          bools[:dont_skip_line] = true
+        elsif s =~ /(?:'(?:[^']?(?:\\')?)*)$/ # verifies multiline string, single quotes
+          single_q = true
+        elsif s =~ /(?:"(?:[^"]?(?:\\")?)*)$/ # verifies multiline string, double quotes
+          double_q = true
+        end
+      else # Just in case something goes wrong process the code without inserting
+        # any information.
+        bools[:dont_skip_line] = false
+      end
+      codes += add_user_codeline(s, i, bools[:dont_skip_line])
+      codes = insert_debug_information(codes, vars, bools[:dont_skip_line])
+      i += 1
+    end
+    codes
+  end
+
+  # Processing of the code if there are multiline strings and no other structures
+  # that need a special treatment. It's done through matching the given code lines
+  # to specific patterns that determine the beginnings and ends of multiline
+  # strings.
+  def multiline_processing(s, booleans)
+    single_q = booleans[:single_q]
+    double_q = booleans[:double_q]
+    dont_skip_line = booleans[:dont_skip_line]
+    if single_q && !double_q
+      if s =~ /^(?:[^']*(?:\\')?)*[^\\]?'\s*(?:;|\+|<<)/
+        single_q = false
+        if s =~ /"(?:[^"]*(?:\\")?)*$/
+          double_q = true
+        elsif s =~ /'(?:[^']*(?:\\')?)*$/
+          single_q = true
+        else
+          dont_skip_line = true
+        end
+      elsif s=~ /^(?:[^']*(?:\\')?)*'\s*(?:#.*)?/
+        single_q = false
+        dont_skip_line = true
+      end
+    elsif double_q && !single_q
+      if s=~ /^(?:[^"]*(?:\\")?)*[^\\]?"\s*(?:;|\+|<<)/
+        double_q = false
+        if s =~ /'(?:[^']*(?:\\')?)*$/
+          single_q = true
+        elsif s =~ /"(?:[^"]*(?:\\")?)*$/
+          double_q = true
+        else
+          dont_skip_line = false
+        end
+      elsif s =~ /^(?:[^"]?(?:\\")?)*[^\\]?"\s*(?:#.*)?/
+        double_q = false
+        dont_skip_line = true
+      end
+    elsif s =~ /;?\s*\w*[!\?]*\s*=\s*"(?:[^"]*(?:\\")?)*$/
+      double_q = true
+      dont_skip_line = false
+    elsif s =~ /;?\s*\w*[!\?]*\s*=\s*'(?:[^']*(?:\\')?)*$/
+      single_q = true
+      dont_skip_line = false
+    end
+    booleans[:single_q] = single_q
+    booleans[:double_q] = double_q
+    booleans[:dont_skip_line] = dont_skip_line
+    return booleans
+  end
+
+  # Regularexpression for validating case-blocks in the code.
   def regex_verify_case_statement
-    /^\s*case\s+(\w+|'\w*'|"\w*")\s*((#+.*\s*)*)?\s*when\s+(\w+|'\w*'|"\w*")\s*(#+.*)?$/
+    /(?:.*;)?\s*case\s*(?:#+.*\s*)*(?:\s:*\w+\s|'(?:[^']?(?:\\')?)*'|"(?:[^"]?(?:\\")?)*")\s*(?:#+.*\s*)*\s*when\s*(?:#+.*\s*)*(?:\s:*\w+\s|'(?:[^']?(?:\\')?)*'|"(?:[^"]?(?:\\")?)*")/
   end
 
-  # Method to add the linenumber for linehighlighting in codemirror previous
-  # to the user's line fo code.
-  def insert_line_number(i)
-    "#{$prefix}_line(#{i})\n"
+  # Verifies if the given line has a complete case statement from the case to
+  # the first when.
+  def regex_verify_case_when
+    /case\s*(?:\s:*\w+\s|'(?:[^']?(?:\\')?)*'|"(?:[^"]?(?:\\")?)*")\s*when/
   end
 
-  # Adds the line of the user's code and a commment with the linenumber.
-  def add_user_code(s, i)
-    s.chomp + " # #{$prefix}_(#{i}#{$prefix}_)\n"
+  # In case there is a when statement after a multiline string and to
+  # prevent that there is a when inside the string mistaken as one of
+  # a case statement.
+  def regex_verify_when_after_multiline
+    /(?:(?:[^']?(?:\\')?)*when(?:[^']?(?:\\')?)*[^\\]'|(?:[^"]?(?:\\")?)*when(?:[^"]?(?:\\")?)*[^\\]?"|(?:[^']?(?:\\')?)*[^\\]'|(?:[^"]?(?:\\")?)*[^\\]");?\s*when/
+  end
+
+  # Verifies start of multiline string with single quotation marks.
+  def regex_verify_when_sq
+    /when\s*(?:'(?:[^']?(?:\\')?)*)$/
+  end
+
+  # Verifies start of multiline string with double quotation marks.
+  def regex_verify_when_dq
+    /when\s*(?:"(?:[^"]?(?:\\")?)*)$/
+  end
+
+  def regex_verify_when_complete
+    /when\s*(?:\s:*\w+\s|'(?:[^']?(?:\\')?)*'|"(?:[^"]?(?:\\")?)*")\s*(?:#.*)?$/
+  end
+
+  # Method to add the linenumber for linehighlighting in codemirror. Previous
+  # to the user's line of code.
+  def insert_line_number(i, dont_skip_line)
+    dont_skip_line ? "#{$prefix}_line(#{i})\n" : ''
+  end
+
+  # Adds the line of the user's code and a commment with the linenumber, doesn't add
+  # a comment if it's processing a multiline string.
+  def add_user_codeline(s, i, no_multiline_string)
+    no_multiline_string ? s.chomp + " # #{$prefix}_(#{i}#{$prefix}_)\n" : s
   end
 
   # Inserts the debug information for tracing the variables during simulation.
   # Also prohibits tracing of our predefined methods.
-  def insert_debug_information(codes, vars)
-    predefined_methods = %w(move turn put take look)
-    vars.each_with_index do |variable, index|
-      unless predefined_methods.include? variable
-        codes += "if defined? #{variable} \n" +
+  def insert_debug_information(codes, vars, dont_skip_line)
+    if dont_skip_line
+      vars.each_with_index do |variable, index|
+        codes += "if defined?(#{variable}) == 'local-variable'\n" +
             "  #{$prefix}_debug(#{variable}, #{index})\n" +
             "end\n"
       end
@@ -135,54 +388,58 @@ class RubyPreprocessor < BasePreprocessor
 $stdout.sync = true
 $stderr.sync = true
 def #{$prefix}_debug(var, ind)
-  puts "\n#{$prefix}_debug_\#{ind}_\#{var}"
+  puts "\n#{$prefix}_debug_\#{ind}_\#{#{$prefix}_escape(var)}"
+end
+def #{$prefix}_escape(var)
+  result = ''
+  var.to_s.each_char do |c|
+    if not c == '\n'
+      result += c
+    end
+  end
+  result
 end
 def move
-  puts "#{$prefix}_move"
+  puts "\n#{$prefix}_move"
 end
 def turn(dir = :back)
   case dir
-    when :right then puts "#{$prefix}_turn_right"
-    when :left then puts "#{$prefix}_turn_left"
-    when :back then puts "#{$prefix}_turn_back"
+    when :right then puts "\n#{$prefix}_turn_right"
+    when :left then puts "\n#{$prefix}_turn_left"
+    when :back then puts "\n#{$prefix}_turn_back"
     else raise(ArgumentError, "unknown argument")
   end
 end
 def put(elem = :buoy)
   case elem
-    when :buoy then puts "#{$prefix}_put_buoy"
-    when :treasure then puts "#{$prefix}_put_treasure"
+    when :buoy then puts "\n#{$prefix}_put_buoy"
+    when :treasure then puts "\n#{$prefix}_put_treasure"
     else raise(ArgumentError, "unknown argument")
   end
 end
 def take
-  puts "#{$prefix}_take"
+  puts "\n#{$prefix}_take"
 end
 def #{$prefix}_line(i)
   puts "\n#{$prefix}_line_\#{i}"
 end
 def look(dir = :here)
   case dir
-    when :right then puts "#{$prefix}_?_look_right"
-    when :left then puts "#{$prefix}_?_look_left"
-    when :here then puts "#{$prefix}_?_look_here"
-    when :back then puts "#{$prefix}_?_look_back"
-    when :front then puts "#{$prefix}_?_look_front"
+    when :right then puts "\n#{$prefix}_?_look_right"
+    when :left then puts "\n#{$prefix}_?_look_left"
+    when :here then puts "\n#{$prefix}_?_look_here"
+    when :back then puts "\n#{$prefix}_?_look_back"
+    when :front then puts "\n#{$prefix}_?_look_front"
     else raise(ArgumentError, "unknown argument")
   end
-  ret = gets
-  if ret.include? "buoy"
-    return :buoy
-  elsif ret.include? "monster"
-    return :monster
-  elsif ret.include? "treasure"
-    return :treasure
-  elsif ret.include? "wave"
-    return :wave
-  elsif ret.include? "border"
-    return :border
-  else
-    return :nothing
+  ret = gets.chomp
+  case ret
+    when 'buoy' then return :buoy
+    when 'monster' then return :monster
+    when 'treasure' then return :treasure
+    when 'wave' then return :wave
+    when 'border' then return :border
+    else return :nothing
   end
 end
 ]
