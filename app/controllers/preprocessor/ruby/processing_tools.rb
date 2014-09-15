b93ee213b324fe9c9d22403d18a94485a2a0552b
amf <amf@informatik.uni-kiel.de>
2014-09-16 00:21:52 +0200

    Cleans ruby regular expressions and adds a last functionality in the case-block processing. More precisely the handling after a normal multiline-string ends and a when or a case beginning is found.
    Also adds a template folder inside the preprocessor with an example preprocessor that includes the 3 required methods and attribute accessor every preprocessor needs for embedding the language. As example it's in the style of ruby.

--- app/controllers/preprocessor/ruby/processing_tools.rb
+++ app/controllers/preprocessor/ruby/processing_tools.rb
@@ -10,46 +10,43 @@ def multiline_processing(s, booleans)
   double_q = booleans[:double_q]
   dont_skip_line = booleans[:dont_skip_line]
   if single_q && !double_q
-    if s =~ /^(?:[^']?(?:\\')?)*[^\\]?'\s*(?:;|\+|<<)/
+    if s =~ regex_multln_sngl_q_end_appnd
       single_q = false
-      if s =~ /^(?:[^']|(?:\\'))*'(?:[^"']?|"(?:[^"]|(?:\\"))*"|'(?:[^']|(?:\\'))*')*"(?:[^"]|(?:\\"))*$/
+      if s =~ regex_multln_sngl_end_dbl_strt
         double_q = true
-      elsif s =~ /^(?:[^']|(?:\\'))*'(?:[^"']?|"(?:[^"]|(?:\\"))*"|'(?:[^']|(?:\\'))*')*'(?:[^']|(?:\\'))*$/
+      elsif s =~ regex_multln_sngl_end_and_strt
         single_q = true
       else
         dont_skip_line = true
       end
-    elsif s=~ /^(?:[^']*(?:\\')?)*'\s*(?:#.*)?/
+    elsif s =~ regex_multiline_single_q_end
       single_q = false
       dont_skip_line = true
     end
   elsif double_q && !single_q
-    if s =~ /^(?:[^"]?(?:\\")?)*[^\\]?"\s*(?:;|\+|<<)/
+    if s =~ regex_multln_dbl_end_appnd
       double_q = false
-      if s =~ /^(?:[^"]|(?:\\"))*"(?:[^"']?|"(?:[^"]|(?:\\"))*"|'(?:[^']|(?:\\'))*')*"(?:[^"]|(?:\\"))*$/
+      if s =~ regex_multln_dbl_end_dbl_strt
         double_q = true
-      elsif s =~ /^(?:[^"]|(?:\\"))*"(?:[^"']?|"(?:[^"]|(?:\\"))*"|'(?:[^']|(?:\\'))*')*'(?:[^']|(?:\\'))*$/
+      elsif s =~ regex_multln_dbl_end_sngl_strt
         single_q = true
       else
         dont_skip_line = true
       end
-    elsif s=~ /^(?:[^"]*(?:\\")?)*"\s*(?:#.*)?/
+    elsif s=~ regex_multiline_double_q_end
       double_q = false
       dont_skip_line = true
     end
-  elsif s =~ /^(?:[^"']?|"(?:[^"]|(?:\\"))*"|'(?:[^']|(?:\\'))*')*"(?:[^"]|(?:\\"))*$/
-    # checks for start of multilinestring with double quotes
+  elsif s =~ regex_start_multiline_double_q
     double_q = true
     dont_skip_line = false
-  elsif s =~ /^(?:[^"']?|"(?:[^"]|(?:\\"))*"|'(?:[^']|(?:\\'))*')*'(?:[^']|(?:\\'))*$/
-    # checks for start of multilinestring with single quotes
+  elsif s =~ regex_start_multiline_single_q
     single_q = true
     dont_skip_line = false
   end
   booleans[:single_q] = single_q
   booleans[:double_q] = double_q
   booleans[:dont_skip_line] = dont_skip_line
-  return booleans
 end
 
 
@@ -62,22 +59,17 @@ def case_block_processing(s, bools)
     bool.each_key do |key|
       bools[key] = bool[key]
     end
-    if bools[:dont_skip_line] && s =~ regex_verify_when_after_multiline
+    if bools[:dont_skip_line] && s =~ regex_verify_when_line_end
       bools[:multiline_string] = false
       bools[:found_case] = true
       bools[:found_when] = true
       bools[:dont_skip_line] = false
-      if s =~ /when\s*(?:#+.*)?$/
-      elsif s =~ regex_verify_when_sq
-        bools[:single_q] = true
-      elsif s =~ regex_verify_when_dq
-        bools[:double_q] = true
-      elsif s =~ regex_verify_when_complete
-        bools[:found_case] = false
-        bools[:found_when] = false
-        bools[:dont_skip_line] = true
-      end
+    elsif bools[:dont_skip_line] && s =~ regex_case_after_multiline
+      bools[:multiline_string] = false
+      bools[:found_case] = true
+      bools[:dont_skip_line] = false
     elsif bools[:dont_skip_line]
+      # if multiline string ends change back to standard processing for 'normal' code lines
       bools[:multiline_string] = false
     end
   elsif !bools[:found_case] && !bools[:found_when] # Standard setting for processing of
@@ -100,31 +92,30 @@ def case_block_processing(s, bools)
         bools[:found_when] = true
         bools[:dont_skip_line] = false
       end
-    elsif s =~ /;?\s*case\s*/
+    elsif s =~ regex_case
       bools[:dont_skip_line] = false
       bools[:found_case] = true
-      if s =~ /case\s*(?:'(?:[^']?(?:\\')?)*)$/ # verifies multiline string
+      if s =~ regex_case_with_sngl_q_multln
         bools[:single_q] = true
         bools[:no_multiline_case_statement] = false
-      elsif s =~ /case\s*(?:"(?:[^"]?(?:\\")?)*)$/ # verifies multiline string
+      elsif s =~ regex_case_with_dbl_q_multln
         bools[:double_q] = true
         bools[:no_multiline_case_statement] = false
       end
-    elsif s =~ /;?\s*\w*[!\?]*\s*=\s*(?:"(?:[^"]*(?:\\")?)*|'(?:[^']*(?:\\')?)*)$/
-      # verifies multiline string
+    elsif s =~ regex_normal_multiline_start
       bools = multiline_processing(s, bools)
       bools[:multiline_string] = true
-    elsif s =~ /'.*when.*'/
+    elsif s =~ regex_verify_when_in_single_q
       # Prevents the word 'when' in single-line strings to be mistaken for a when
       # of a case statement.
 
-    elsif s =~ /".*when.*"/
+    elsif s =~ regex_verify_when_in_double_q
 
-    elsif s =~ /when/
+    elsif s =~ regex_when
       bools[:found_case] = true
       bools[:found_when] = true
       bools[:dont_skip_line] = false
-      if s =~ /when\s*(?:#+.*)?$/
+      if s =~ regex_verify_when_line_end
       elsif s =~ regex_verify_when_sq
         bools[:single_q] = true
       elsif s =~ regex_verify_when_dq
@@ -137,10 +128,10 @@ def case_block_processing(s, bools)
     end
   elsif bools[:found_case] && !bools[:found_when] # beginning of a case statement has been found
     if !bools[:no_multiline_case_statement] # found a multiline string for statement
-      if bools[:single_q] && s =~ /(?:[^']?(?:\\')?)*[^\\]?'/
+      if bools[:single_q] && s =~ regex_single_q_end
         bools[:single_q] = false
         bools[:no_multiline_case_statement] = true
-        if s =~ /when\s*(?:#+.*)?$/
+        if s =~ regex_verify_when_line_end
           bools[:found_when] = true
         elsif s =~ regex_verify_when_sq
           bools[:single_q] = true
@@ -152,10 +143,10 @@ def case_block_processing(s, bools)
           bools[:dont_skip_line] = true
           bools[:found_case] = false
         end
-      elsif bools[:double_q] && s =~ /(?:[^"]?(?:\\")?)*[^\\]?"\s*/
+      elsif bools[:double_q] && s =~ regex_double_q_end
         bools[:double_q] = false
         bools[:no_multiline_case_statement] = true
-        if s =~ /when\s*(?:#+.*)?$/
+        if s =~ regex_verify_when_line_end
           bools[:found_when] = true
         elsif s=~ regex_verify_when_sq
           bools[:single_q] = true
@@ -168,9 +159,9 @@ def case_block_processing(s, bools)
           bools[:found_case] = false
         end
       end
-    elsif s =~ /\s*(?::*\w+\s|'(?:[^']?(?:\\')?)*'|"(?:[^"]?(?:\\")?)*")?\s*when/
+    elsif s =~ regex_case_end_complete_when
       # found rest of case statement and the following when
-      if s =~ /when\s*(?:#+.*)?$/
+      if s =~ regex_verify_when_line_end
         bools[:found_when] = true
       elsif s=~ regex_verify_when_sq
         bools[:single_q] = true
@@ -182,23 +173,23 @@ def case_block_processing(s, bools)
         bools[:dont_skip_line] = true
         bools[:found_case] = false
       end
-    elsif s =~ /(?:'(?:[^']?(?:\\')?)*)$/ # verifies beginning of case multiline string, single quoted
+    elsif s =~ regex_single_q_multiline_start # verifies beginning of case multiline string, single quoted
       bools[:single_q] = true
       bools[:no_multiline_case_statement] = false
-    elsif s =~ /(?:"(?:[^"]?(?:\\")?)*)$/ # verifies beginning of case multiline string, double quoted
+    elsif s =~ regex_double_q_multiline_start # verifies beginning of case multiline string, double quoted
       bools[:double_q] = true
       bools[:no_multiline_case_statement] = false
-    elsif s =~ /when/
+    elsif s =~ regex_when
       bools[:found_when] = true
     end
   elsif bools[:found_case] && bools[:found_when]
-    if bools[:single_q] && s =~ /(?:[^']?(?:\\')?)*[^\\]?'\s*(?:#+.*)?$/
+    if bools[:single_q] && s =~ regex_single_q_end
       # verifies end of multiline string, single quotes
       bools[:found_case] = false
       bools[:found_when] = false
       bools[:single_q] = false
       bools[:dont_skip_line] = true
-    elsif bools[:double_q] && s =~ /(?:[^"]?(?:\\")?)*[^\\]?"\s*(?:#+.*)?$/
+    elsif bools[:double_q] && s =~ regex_double_q_end
       # verifies end of multiline string, double quotes
       bools[:found_case] = false
       bools[:found_when] = false
@@ -206,14 +197,14 @@ def case_block_processing(s, bools)
       bools[:dont_skip_line] = true
     elsif bools[:double_q] || bools[:single_q]
 
-    elsif s=~ /(?:\s:*\w+\s|'(?:[^']?(?:\\')?)*'|"(?:[^"]?(?:\\")?)*")\s*(?:#.*)?/
+    elsif s=~ regex_verify_complete_when
       # verifies end of first when statement
       bools[:found_case] = false
       bools[:found_when] = false
       bools[:dont_skip_line] = true
-    elsif s =~ /(?:'(?:[^']?(?:\\')?)*)$/ # verifies multiline string, single quotes
+    elsif s =~ regex_single_q_multiline_start # verifies multiline string, single quotes
       bools[:single_q] = true
-    elsif s =~ /(?:"(?:[^"]?(?:\\")?)*)$/ # verifies multiline string, double quotes
+    elsif s =~ regex_double_q_multiline_start # verifies multiline string, double quotes
       bools[:double_q] = true
     end
   else # Just in case something goes wrong process the code without inserting
---
+++ app/controllers/preprocessor/template/example_preprocessor.rb
@@ -0,0 +1,75 @@
+# -*- encoding : utf-8 -*-
+class ExamplePreprocessor
+
+  # This flag needs to be set, so the communication between server
+  # and client knows, if the packets are sent at the end of the
+  # line of user's code or at the beginning.
+  attr :line_first
+
+  # For example here the initializer of preprocessor for ruby.
+  # The only attribute that needs to be set is @line_first, which
+  # is accessed from other points outside the preprocessor.
+  # Nontheless it's recommended to set instance_variables like
+  # @code and flags like @syntaxflag for easier access inside
+  # the preprocessor.
+  def initialize(code, tracing_vars)
+    @line_first = true
+    @operationlist = []
+    @end_break = ''
+    @beg_break = ''
+    @filename = "#{$prefix}_code.rb"
+    @syntaxflag = true
+    @code = code
+    @tracing_vars = tracing_vars
+    # @processed_code = process_code(code)
+    # not
+  end
+
+  # Returns an array of commands (in hashes) for the VM. Here
+  # for example it's the command array for ruby.
+  # A list of the default-commands can be found in
+  # /app/controllers/tools/commands_for_vm.rb
+  def commands_for_vm
+    [{:write_file => {:filename => @filename, :content => @code}},
+     {:execute => {:command => "ruby -c #{@filename}", :stdout => 'checksuccess', :stderr => 'checkerror'}}]
+    # in this case execution is a syntax check, real execution is
+    # handled in postprocess_print
+  end
+
+  # This method handles the thrown errors of the user codes execution.
+  # Here for example it's the handling for ruby.
+  # If syntax check succeeds a call is send for execution of processed
+  # code. Also if there is an error whilst syntax check the error is
+  # processed by self defined function postprocess_execute
+  def postprocess_print(send, type, line)
+    if type == 'checksuccess'
+      @process_code = process_code(@code, @tracing_vars)
+      send.call([{:write_file => {:filename => @filename, :content => @process_code}}, {:execute => {:command => "ruby #{@filename}"}}, {:exit => {}}])
+      {:type => :no}
+    elsif type == 'checkerror'
+      if @syntaxflag
+        send.call([{:exit => {:successful => false, :message => 'Syntaxfehler'}}])
+        @syntaxflag = false
+      end
+      return {:type => :error, :message => line}
+    else
+      postprocess_execute(line)
+    end
+  end
+
+  # Self defined method for message processing, no need to be called
+  # postprocess_execute (free naming).
+  def postprocess_execute(line)
+    # here is the individual message processing located
+    line
+  end
+
+  # Self defined method for processing of user code.
+  # Handles insertion of debug- or linehighlighting-information.
+  def process_code(code, tracing_vars)
+    # insert information in code here
+    tracing_vars
+    code
+  end
+
+end
