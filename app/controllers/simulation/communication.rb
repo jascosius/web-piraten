33c5a602dc7828b81d027144a96ea8004f7e7e8d
Mathis Neumann <mne@informatik.uni-kiel.de>
2014-09-22 02:21:53 +0300

    remove unnecessary performance logs

--- app/controllers/simulation/communication.rb
+++ app/controllers/simulation/communication.rb
@@ -44,7 +44,7 @@ def communicate_with_vm(tracing_vars)
     # PERFORMANCE_LOGGER.store :vm_get_line, perf, Time.now
     line = line.force_encoding('utf-8')
 
-    perf = Time.now
+    # perf = Time.now
     unless line.empty?
       array = line.split('_') #a command looks like $prefix_function_params or $prefix_?_function_params
       if array[0] == $prefix #is the line a command?
@@ -57,7 +57,7 @@ def communicate_with_vm(tracing_vars)
         print!(:log, line) #without $prefix this must be a print from the user
       end
     end
-    PERFORMANCE_LOGGER.store :vm_zeile_abarbeiten, perf, Time.now
+    # PERFORMANCE_LOGGER.store :vm_zeile_abarbeiten, perf, Time.now
   end
   @vm.puts([{:stop => {}}].to_json)
 
