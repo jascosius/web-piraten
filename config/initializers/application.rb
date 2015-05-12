ddc9f66a978e3af8f4ff06f8d71e84c9c386cbfd
Jascosius <git@jascosius.de>
2015-05-12 20:38:19 +0200

    prepair for github, try to move config, fix wrong path

--- config/initializers/application.rb
+++ config/initializers/application.rb
@@ -1,5 +1,5 @@
 if Rails.env.production?
-  require '../../env_conf/application'
+  require File.expand_path('env_conf/application')
   #Settings for production
   #VM_TIMEOUT = 60 #timeout time for the programm to execute, change in the vm as well
   #VM_PORT = 12340 #port to connect to the vm
@@ -7,9 +7,10 @@ if Rails.env.production?
   #VM_PREFIX = 'CkyUHZVL3q'
   #PEPPER_PATH_RAILS = Dir.home + '/.webpiraten-pepper' #path to pepper
 else
+  require File.expand_path('env_conf/application')
   #Settings for development
-  VM_TIMEOUT = 60 #timeout time for the programm to execute, change in the vm as well
-  VM_PORT = 12340 #port to connect to the vm
-  VM_HOST = 'localhost' #chevalblanc.informatik.uni-kiel.de' #host to connect to the vm #localhost
-  VM_PREFIX = 'CkyUHZVL3q'
+  #VM_TIMEOUT = 60 #timeout time for the programm to execute, change in the vm as well
+  #VM_PORT = 12340 #port to connect to the vm
+  #VM_HOST = 'localhost' #chevalblanc.informatik.uni-kiel.de' #host to connect to the vm #localhost
+  #VM_PREFIX = 'CkyUHZVL3q'
 end
